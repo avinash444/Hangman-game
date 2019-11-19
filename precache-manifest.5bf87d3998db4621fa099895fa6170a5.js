dba82573d39dd9c026b9611de4dcffc7cf852413
avinash <nashchowdary@gmail.com>
2019-11-19 09:49:34 +0530

    Updates

---
+++ precache-manifest.5bf87d3998db4621fa099895fa6170a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c158f3ec2aa3734c6c61c561b7e8e442",
+    "url": "/hangman-game/index.html"
+  },
+  {
+    "revision": "437dcb00f547bc602d6a",
+    "url": "/hangman-game/static/css/main.8f95d9fe.chunk.css"
+  },
+  {
+    "revision": "7710a7022d5ac854b1c2",
+    "url": "/hangman-game/static/js/2.f2a6f2c4.chunk.js"
+  },
+  {
+    "revision": "437dcb00f547bc602d6a",
+    "url": "/hangman-game/static/js/main.cb3585aa.chunk.js"
+  },
+  {
+    "revision": "4cb42b9f5f4eff4d6ab7",
+    "url": "/hangman-game/static/js/runtime-main.f75ab5e6.js"
+  },
+  {
+    "revision": "932a05d32533ddf104c7e3f3874fcb7d",
+    "url": "/hangman-game/static/media/hangman1.932a05d3.png"
+  },
+  {
+    "revision": "656e9ecedac92c1c29dd965822233e33",
+    "url": "/hangman-game/static/media/hangman2.656e9ece.png"
+  },
+  {
+    "revision": "0d98c1db6d9ebb4087bedab1aa7cd468",
+    "url": "/hangman-game/static/media/hangman3.0d98c1db.png"
+  },
+  {
+    "revision": "28889e2f8e16394f3aae7a5ff20045bb",
+    "url": "/hangman-game/static/media/hangman4.28889e2f.png"
+  },
+  {
+    "revision": "0e949d9e0a6f5d6ae87501a8b1398747",
+    "url": "/hangman-game/static/media/hangman5.0e949d9e.png"
+  },
+  {
+    "revision": "73eed85381a7229948dd912315b7940d",
+    "url": "/hangman-game/static/media/hangman6.73eed853.png"
+  },
+  {
+    "revision": "d584fd827c80d18ee721a82e87782778",
+    "url": "/hangman-game/static/media/hangman7.d584fd82.png"
+  }
+]);

fe51258ce16942a303508e84225e200d7a9bc2cc
avinash <nashchowdary@gmail.com>
2019-11-19 10:39:50 +0530

    image background fix

--- src/components/Diagrams.js
+++ src/components/Diagrams.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Diagram = ({ diagramId }) => {
   return (
     <div>
-      <div data-diagramId={diagramId} className='diagram'></div>
+      <div data-diagramid={diagramId} className='diagram'></div>
     </div>
   )
 }
